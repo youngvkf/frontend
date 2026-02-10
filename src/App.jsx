e6cd22dc734155f2aea2714a2632d0bb932928ce
YoungM <e2476054@ewha.ac.kr>
2026-02-10 19:47:54 +0900

    배포 준비

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import MentorMenteePlannerApp from "./MentorMenteePlannerApp";
 import Login from './login';
 
 export default function App() {
   return (
     <Routes>
+      {/* ✅ 추가: 루트('/') 처리 */}
+      <Route path="/" element={<Navigate to="/login" replace />} />
+
       <Route path="/login" element={<Login />} />
       <Route path="/planner" element={<MentorMenteePlannerApp />} />
     </Routes>
